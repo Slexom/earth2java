3bbcbe0c40b79dac6e87c81ed75cd3be00fc9457
Carmelo Buscemi <b.carmelo.92@gmail.com>
2020-06-01 17:57:15 +0200

    New Mobs Ashen Cow, new textures for Wooly Cow

---
+++ src/main/java/net/slexom/earthtojavamobs/entity/AshenCowEntity.java
@@ -0,0 +1,104 @@
+
+package net.slexom.earthtojavamobs.entity;
+
+import net.minecraft.client.renderer.entity.CowRenderer;
+import net.minecraft.entity.*;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DeferredWorkQueue;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLPlayMessages;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.slexom.earthtojavamobs.EarthtojavamobsModElements;
+
+@EarthtojavamobsModElements.ModElement.Tag
+public class AshenCowEntity extends EarthtojavamobsModElements.ModElement {
+    public static EntityType entity = null;
+
+    public AshenCowEntity(EarthtojavamobsModElements instance) {
+        super(instance, 9);
+        FMLJavaModLoadingContext.get().getModEventBus().register(this);
+    }
+
+    @Override
+    public void initElements() {
+        entity = (EntityType.Builder.<CustomEntity>create(CustomEntity::new, EntityClassification.CREATURE).setShouldReceiveVelocityUpdates(true)
+                .setTrackingRange(64).setUpdateInterval(3).setCustomClientFactory(CustomEntity::new).size(0.9f, 1.4f)).build("ashen_cow")
+                .setRegistryName("ashen_cow");
+        elements.entities.add(() -> entity);
+        elements.items.add(() -> new SpawnEggItem(entity, 0x3c3c49, 0x898491, new Item.Properties().group(ItemGroup.MISC)).setRegistryName("ashen_cow"));
+    }
+
+    @Override
+    public void init(FMLCommonSetupEvent event) {
+        DeferredWorkQueue.runLater(new Runnable() {
+            @Override
+            public void run() {
+                for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
+                    boolean biomeCriteria = false;
+                    if (ForgeRegistries.BIOMES.getKey(biome).equals(new ResourceLocation("plains")))
+                        biomeCriteria = true;
+                    if (!biomeCriteria)
+                        continue;
+                    biome.getSpawns(EntityClassification.CREATURE).add(new Biome.SpawnListEntry(entity, 20, 4, 4));
+                }
+                EntitySpawnPlacementRegistry.register(entity, EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+                        AnimalEntity::canAnimalSpawn);
+            }
+        });
+    }
+
+    @SubscribeEvent
+    public void registerModels(ModelRegistryEvent event) {
+        RenderingRegistry.registerEntityRenderingHandler(entity, renderManager -> new CowRenderer(renderManager) {
+            @Override
+            public ResourceLocation getEntityTexture(CowEntity entity) {
+                return new ResourceLocation("earthtojavamobs:textures/mobs/cow/ashen_cow/ashen_cow.png");
+            }
+        });
+    }
+
+    public static class CustomEntity extends CowEntity {
+        public CustomEntity(FMLPlayMessages.SpawnEntity packet, World world) {
+            this(entity, world);
+        }
+
+        public CustomEntity(EntityType<CustomEntity> type, World world) {
+            super(type, world);
+            experienceValue = 0;
+            setNoAI(false);
+        }
+
+        @Override
+        protected void registerGoals() {
+            super.registerGoals();
+        }
+
+        @Override
+        public CreatureAttribute getCreatureAttribute() {
+            return CreatureAttribute.UNDEFINED;
+        }
+
+        protected void dropSpecialItems(DamageSource source, int looting, boolean recentlyHitIn) {
+            super.dropSpecialItems(source, looting, recentlyHitIn);
+        }
+
+        @Override
+        public CowEntity createChild(AgeableEntity ageable) {
+            return (CustomEntity) entity.create(this.world);
+        }
+
+    }
+}
