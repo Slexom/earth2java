3673f637a843e010b0ea70873d0a53ed8540fb1c
Carmelo Buscemi <b.carmelo.92@gmail.com>
2020-07-03 23:15:52 +0200

    [2.1.0] Restored mud reaction;

--- src/main/java/net/slexom/earthtojavamobs/block/MudBlock.java
+++ src/main/java/net/slexom/earthtojavamobs/block/MudBlock.java
@@ -1,7 +1,16 @@
 package net.slexom.earthtojavamobs.block;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.fluid.FlowingFluid;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Direction;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
+import net.slexom.earthtojavamobs.EarthtojavamobsMod;
 
 import java.util.function.Supplier;
 
@@ -9,6 +18,29 @@ public class MudBlock extends FlowingFluidBlock {
 
     public MudBlock(Supplier<? extends FlowingFluid> supplier, Properties p_i48368_1_) {
         super(supplier, p_i48368_1_);
+    }
+
+    private void triggerMixEffects(IWorld worldIn, BlockPos pos) {
+        worldIn.playEvent(1501, pos, 0);
+    }
+
+    public boolean reactWithNeighbors(World worldIn, BlockPos pos, BlockState state) {
+        ResourceLocation mudTag = new ResourceLocation(EarthtojavamobsMod.MOD_ID, "mud");
+        if (this.getFluid().isIn(FluidTags.getCollection().getOrCreate(mudTag))) {
+            boolean flag = false;
+            for (Direction direction : Direction.values()) {
+                if (worldIn.getFluidState(pos.offset(direction)).isTagged(FluidTags.LAVA)) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
+                worldIn.setBlockState(pos, net.minecraftforge.event.ForgeEventFactory.fireFluidPlaceBlockEvent(worldIn, pos, pos, Blocks.DIRT.getDefaultState()));
+                this.triggerMixEffects(worldIn, pos);
+                return false;
+            }
+        }
+        return true;
     }
 
 }
