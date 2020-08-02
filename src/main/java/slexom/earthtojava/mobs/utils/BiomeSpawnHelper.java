4c37fe8bd4ed8b6c9716789a0d0b5f0bb24a24bf
Carmelo Buscemi <b.carmelo.92@gmail.com>
2020-08-02 16:56:10 +0200

    Improved SpawnBiomeHelper to accept Biome.Category value and black listed identifier

--- src/main/java/slexom/earthtojava/mobs/utils/BiomeSpawnHelper.java
+++ src/main/java/slexom/earthtojava/mobs/utils/BiomeSpawnHelper.java
@@ -7,12 +7,12 @@ import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.WaterCreatureEntity;
 import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.util.Util;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.Biomes;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -89,16 +89,92 @@ public final class BiomeSpawnHelper {
         return Stream.of(biomes).flatMap(Stream::of).toArray(String[]::new);
     }
 
-    private static void setSpawnBiomes(EntityType entity, String[] spawnBiomes, int weight, int minGroupSize, int maxGroupSize, SpawnGroup classification) {
-        Registry.BIOME.forEach(biome -> addToBiome(biome,  entity,   spawnBiomes,  weight,  minGroupSize,   maxGroupSize,  classification));
-        RegistryEntryAddedCallback.event(Registry.BIOME).register((i, identifier, biome) -> addToBiome(biome,  entity,   spawnBiomes,  weight,  minGroupSize,   maxGroupSize,  classification));
+    public static String[] getBiomesListFromBiomeCategories(Biome.Category... types) {
+        return Stream.of(types).flatMap(Stream::of).map(Biome.Category::getName).toArray(String[]::new);
     }
 
-    private static void addToBiome(Biome biome, EntityType<?> entity, String[] spawnBiomes, int weight, int minGroupSize, int maxGroupSize, SpawnGroup classification) {
-        boolean biomeCriteria = Arrays.asList(spawnBiomes).contains(Registry.BIOME.getId(biome).toString());
-        if (biomeCriteria) {
-            biome.getEntitySpawnList(classification).add(new Biome.SpawnEntry(entity, weight, minGroupSize, maxGroupSize));
+//    private static void setSpawnBiomes(EntityType entity, String[] spawnBiomes, int weight, int minGroupSize, int maxGroupSize, SpawnGroup classification) {
+//        Registry.BIOME.forEach(biome -> addToBiome(biome,  entity,   spawnBiomes,  weight,  minGroupSize,   maxGroupSize,  classification));
+//        RegistryEntryAddedCallback.event(Registry.BIOME).register((i, identifier, biome) -> addToBiome(biome,  entity,   spawnBiomes,  weight,  minGroupSize,   maxGroupSize,  classification));
+//    }
+
+    private static void setSpawnBiomes(EntityType<?> entity, String[] spawnBiomes, int weight, int minGroupSize, int maxGroupSize, SpawnGroup classification) {
+        List<String> blackList = Arrays.stream(spawnBiomes).filter(id -> id.contains("!")).collect(Collectors.toList());
+        List<String> spawnList = expandSpawnList(Arrays.stream(spawnBiomes).filter(id -> !id.contains("!")).collect(Collectors.toList()));
+        blackList.replaceAll(s -> s.replace("!", ""));
+        spawnList.removeAll(blackList);
+        addEntityToBiomes(entity, spawnList, minGroupSize, maxGroupSize, classification, weight);
+    }
+
+    private static List<String> expandSpawnList(List<String> spawnList) {
+        List<String> biomes = new ArrayList<>(Collections.emptyList());
+        List<String> biomeCategories = new ArrayList<>(Collections.emptyList());
+        List<String> biomesFromCategories = new ArrayList<>(Collections.emptyList());
+        spawnList.forEach(identifier -> {
+            if (isBiomeCategory(identifier)) {
+                biomeCategories.add(identifier);
+            } else {
+                biomes.add(identifier);
+            }
+        });
+        for (String biomeCategory : biomeCategories) {
+            Registry.BIOME.forEach(biome -> {
+                if (biome.getCategory().toString().toUpperCase().equals(biomeCategory.toUpperCase())) {
+                    biomesFromCategories.add(Registry.BIOME.getId(biome).toString());
+                }
+            });
         }
+        return Stream.concat(biomes.stream(), biomesFromCategories.stream()).collect(Collectors.toList());
+    }
+
+    private static boolean isBiomeCategory(String identifier) {
+        return identifier.split(":").length == 1;
+    }
+
+//    private static void removeEntityFromBiomes(EntityType<?> entity, List<String> blackList, int weight, int minGroupSize, int maxGroupSize, SpawnGroup classification) {
+//        for (String identifier : blackList) {
+//            Biome vanillaBiome = Registry.BIOME.get(new Identifier(identifier));
+//            if (vanillaBiome != null) {
+//                removeFromBiome(vanillaBiome, entity, weight, minGroupSize, maxGroupSize, classification);
+//            }
+//            RegistryEntryAddedCallback.event(Registry.BIOME).register((i, registryName, biome) -> {
+//                if (registryName.toString().equals(identifier)) {
+//                    removeFromBiome(biome, entity, weight, minGroupSize, maxGroupSize, classification);
+//                }
+//            });
+//        }
+//    }
+
+    private static void addEntityToBiomes(EntityType<?> entity, List<String> spawnList, int minGroupSize, int maxGroupSize, SpawnGroup classification, int weight) {
+        for (String identifier : spawnList) {
+            String[] splitted = identifier.split(":");
+            if (splitted.length == 2) {
+                Registry.BIOME.forEach(biome -> {
+                    if (Registry.BIOME.getId(biome).toString().equals(identifier)) {
+                        addToBiome(biome, entity, weight, minGroupSize, maxGroupSize, classification);
+                    }
+                });
+                RegistryEntryAddedCallback.event(Registry.BIOME).register((i, registryName, biome) -> {
+                    if (registryName.toString().equals(identifier)) {
+                        addToBiome(biome, entity, weight, minGroupSize, maxGroupSize, classification);
+                    }
+                });
+            }
+            if (splitted.length == 1) {
+                Registry.BIOME.forEach(biome -> addToBiomeCategory(biome, identifier, entity, weight, minGroupSize, maxGroupSize, classification));
+                RegistryEntryAddedCallback.event(Registry.BIOME).register((i, registryName, biome) -> addToBiomeCategory(biome, identifier, entity, weight, minGroupSize, maxGroupSize, classification));
+            }
+        }
+    }
+
+    private static void addToBiomeCategory(Biome biome, String identifier, EntityType<?> entity, int weight, int minGroupSize, int maxGroupSize, SpawnGroup classification) {
+        if (biome.getCategory().toString().toUpperCase().equals(identifier.toUpperCase())) {
+            addToBiome(biome, entity, weight, minGroupSize, maxGroupSize, classification);
+        }
+    }
+
+    private static void addToBiome(Biome biome, EntityType<?> entity, int weight, int minGroupSize, int maxGroupSize, SpawnGroup classification) {
+        biome.getEntitySpawnList(classification).add(new Biome.SpawnEntry(entity, weight, minGroupSize, maxGroupSize));
     }
 
     public static <T extends AnimalEntity> void setCreatureSpawnBiomes(EntityType<T> entity, String[] spawnBiomes, int weight, int minGroupCountIn, int maxGroupCountIn) {
