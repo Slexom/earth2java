04ab9df6e08674ebcab0fefcb3d9d2ceaf986a28
Carmelo Buscemi <b.carmelo.92@gmail.com>
2020-08-13 22:59:31 +0200

    Updated version

--- src/main/java/slexom/earthtojava/mobs/init/FeatureInit.java
+++ src/main/java/slexom/earthtojava/mobs/init/FeatureInit.java
@@ -5,6 +5,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.decorator.ChanceDecoratorConfig;
+import net.minecraft.world.gen.decorator.CountNoiseDecoratorConfig;
 import net.minecraft.world.gen.decorator.Decorator;
 import net.minecraft.world.gen.decorator.DecoratorConfig;
 import net.minecraft.world.gen.feature.*;
@@ -22,7 +23,7 @@ public class FeatureInit {
     public static ConfiguredFeature<?, ?> ORE_RUBY_CONFIGURED_FEATURE;
 
     public static void init() {
-        FLOWER_BUTTERCUP_CONFIGURED_FEATURE = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Earth2JavaMod.MOD_ID, "flower_buttercup"), Feature.FLOWER.configure((new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockInit.BUTTERCUP.getDefaultState()), SimpleBlockPlacer.INSTANCE)).tries(64).build()));
+        FLOWER_BUTTERCUP_CONFIGURED_FEATURE = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Earth2JavaMod.MOD_ID, "flower_buttercup"), (Feature.FLOWER.configure((new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BlockInit.BUTTERCUP.getDefaultState()), SimpleBlockPlacer.INSTANCE)).tries(64).build()).decorate(ConfiguredFeatures.Decorators.SPREAD_32_ABOVE).decorate(ConfiguredFeatures.Decorators.HEIGHTMAP).spreadHorizontally()).decorate(Decorator.COUNT_NOISE.configure(new CountNoiseDecoratorConfig(-0.8D, 15, 4))));
         MUD_LAKE_CONFIGURED_FEATURE = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Earth2JavaMod.MOD_ID, "lake_mud"), Feature.LAKE.configure(new SingleStateFeatureConfig(BlockInit.MUD_BLOCK.getDefaultState())).decorate(Decorator.WATER_LAKE.configure(new ChanceDecoratorConfig(config.mudLakeFrequency))));
         ORE_RUBY_CONFIGURED_FEATURE = Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Earth2JavaMod.MOD_ID, "ruby_ore"), Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockInit.RUBY_ORE.getDefaultState(), 8)).decorate(Decorator.EMERALD_ORE.configure(DecoratorConfig.DEFAULT)));
     }
