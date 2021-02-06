5f894acc54699c0045cda36687a5ebaec18db92f
Carmelo Buscemi <b.carmelo.92@gmail.com>
2021-02-06 16:37:57 +0100

    feat: Rainbow Sheep sounds

--- src/main/java/slexom/earthtojava/mobs/entity/passive/RainbowSheepEntity.java
+++ src/main/java/slexom/earthtojava/mobs/entity/passive/RainbowSheepEntity.java
@@ -2,12 +2,24 @@ package slexom.earthtojava.mobs.entity.passive;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
 import slexom.earthtojava.mobs.entity.base.E2JBaseMonoColorSheepEntity;
 import slexom.earthtojava.mobs.init.BlockInit;
+import slexom.earthtojava.mobs.init.SoundEventsInit;
 
 public class RainbowSheepEntity extends E2JBaseMonoColorSheepEntity<RainbowSheepEntity> {
+
     public RainbowSheepEntity(EntityType<? extends RainbowSheepEntity> type, World world) {
-        super(type, world, new ItemStack( BlockInit.RAINBOW_WOOL));
+        super(type, world, new ItemStack(BlockInit.RAINBOW_WOOL));
     }
+
+    protected SoundEvent getAmbientSound() {
+        return SoundEventsInit.RAINBOW_SHEEP_AMBIENT;
+    }
+
+    protected SoundEvent getDeathSound() {
+        return SoundEventsInit.RAINBOW_SHEEP_DEATH;
+    }
+
 }
--- src/main/java/slexom/earthtojava/mobs/init/SoundEventsInit.java
+++ src/main/java/slexom/earthtojava/mobs/init/SoundEventsInit.java
@@ -12,18 +12,22 @@ public class SoundEventsInit {
     public static final String BONE_SPIDER_SPIT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.BONE_SPIDER_REGISTRY_NAME, "spit");
     public static final String BONE_SPIDER_WALK_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.BONE_SPIDER_REGISTRY_NAME, "walk");
     public static final String FANCY_CHICKEN_AMBIENT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.FANCY_CHICKEN_REGISTRY_NAME, "ambient");
-    public static final String FANCY_CHICKEN_HURT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.FANCY_CHICKEN_REGISTRY_NAME, "hurt");
     public static final String FANCY_CHICKEN_DEATH_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.FANCY_CHICKEN_REGISTRY_NAME, "death");
+    public static final String FANCY_CHICKEN_HURT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.FANCY_CHICKEN_REGISTRY_NAME, "hurt");
     public static final String FANCY_CHICKEN_FLEE_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.FANCY_CHICKEN_REGISTRY_NAME, "flee");
+    public static final String RAINBOW_SHEEP_AMBIENT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.RAINBOW_SHEEP_REGISTRY_NAME, "ambient");
+    public static final String RAINBOW_SHEEP_DEATH_REGISTRY_NAME = getEntitySoundEventName( EntityTypesInit.RAINBOW_SHEEP_REGISTRY_NAME, "death");
     public static SoundEvent BONE_SPIDER_AMBIENT;
     public static SoundEvent BONE_SPIDER_BONE_STAB;
     public static SoundEvent BONE_SPIDER_DEATH;
     public static SoundEvent BONE_SPIDER_SPIT;
     public static SoundEvent BONE_SPIDER_WALK;
     public static SoundEvent FANCY_CHICKEN_AMBIENT;
-    public static SoundEvent FANCY_CHICKEN_HURT;
     public static SoundEvent FANCY_CHICKEN_DEATH;
+    public static SoundEvent FANCY_CHICKEN_HURT;
     public static SoundEvent FANCY_CHICKEN_FLEE;
+    public static SoundEvent RAINBOW_SHEEP_AMBIENT;
+    public static SoundEvent RAINBOW_SHEEP_DEATH;
 
     public static void init() {
         BONE_SPIDER_AMBIENT = RegisterHelper.registerSoundEvent(BONE_SPIDER_AMBIENT_REGISTRY_NAME);
@@ -32,9 +36,11 @@ public class SoundEventsInit {
         BONE_SPIDER_WALK = RegisterHelper.registerSoundEvent(BONE_SPIDER_WALK_REGISTRY_NAME);
         BONE_SPIDER_SPIT = RegisterHelper.registerSoundEvent(BONE_SPIDER_SPIT_REGISTRY_NAME);
         FANCY_CHICKEN_AMBIENT = RegisterHelper.registerSoundEvent(FANCY_CHICKEN_AMBIENT_REGISTRY_NAME);
-        FANCY_CHICKEN_HURT = RegisterHelper.registerSoundEvent(FANCY_CHICKEN_HURT_REGISTRY_NAME);
         FANCY_CHICKEN_DEATH  =RegisterHelper.registerSoundEvent(FANCY_CHICKEN_DEATH_REGISTRY_NAME);
-        FANCY_CHICKEN_FLEE  =RegisterHelper.registerSoundEvent(FANCY_CHICKEN_FLEE_REGISTRY_NAME);
+        FANCY_CHICKEN_HURT = RegisterHelper.registerSoundEvent(FANCY_CHICKEN_HURT_REGISTRY_NAME);
+        FANCY_CHICKEN_FLEE  =RegisterHelper.registerSoundEvent(FANCY_CHICKEN_FLEE_REGISTRY_NAME);;
+        RAINBOW_SHEEP_AMBIENT = RegisterHelper.registerSoundEvent(RAINBOW_SHEEP_AMBIENT_REGISTRY_NAME);
+        RAINBOW_SHEEP_DEATH  =RegisterHelper.registerSoundEvent( RAINBOW_SHEEP_DEATH_REGISTRY_NAME);
     }
 
     private static String getEntitySoundEventName(String registryName, String type) {
