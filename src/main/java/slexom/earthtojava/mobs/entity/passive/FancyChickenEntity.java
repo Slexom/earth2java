8ed6ecfb2a86743895e42096416781ee7eccd2ff
Carmelo Buscemi <b.carmelo.92@gmail.com>
2021-02-06 15:54:38 +0100

    feat: Fancy Chicken sounds; Fancy Chicken flee from pigs ai goal

--- src/main/java/slexom/earthtojava/mobs/entity/passive/FancyChickenEntity.java
+++ src/main/java/slexom/earthtojava/mobs/entity/passive/FancyChickenEntity.java
@@ -1,13 +1,36 @@
-
 package slexom.earthtojava.mobs.entity.passive;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
+import slexom.earthtojava.mobs.entity.ai.goal.FancyChickenFleeFromPigEntityGoal;
 import slexom.earthtojava.mobs.entity.base.E2JBaseChickenEntity;
+import slexom.earthtojava.mobs.init.SoundEventsInit;
 
 public class FancyChickenEntity extends E2JBaseChickenEntity<FancyChickenEntity> {
 
     public FancyChickenEntity(EntityType<FancyChickenEntity> type, World world) {
         super(type, world);
     }
+
+    @Override
+    protected void initGoals() {
+        super.initGoals();
+        this.goalSelector.add(3, new FancyChickenFleeFromPigEntityGoal(this, PigEntity.class, 6.0F, 1.0D, 1.2D));
+    }
+
+    protected SoundEvent getAmbientSound() {
+        return SoundEventsInit.FANCY_CHICKEN_AMBIENT;
+    }
+
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return SoundEventsInit.FANCY_CHICKEN_HURT;
+    }
+
+    protected SoundEvent getDeathSound() {
+        return SoundEventsInit.FANCY_CHICKEN_DEATH;
+    }
+
 }
--- src/main/java/slexom/earthtojava/mobs/init/SoundEventsInit.java
+++ src/main/java/slexom/earthtojava/mobs/init/SoundEventsInit.java
@@ -11,11 +11,19 @@ public class SoundEventsInit {
     public static final String BONE_SPIDER_DEATH_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.BONE_SPIDER_REGISTRY_NAME, "death");
     public static final String BONE_SPIDER_SPIT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.BONE_SPIDER_REGISTRY_NAME, "spit");
     public static final String BONE_SPIDER_WALK_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.BONE_SPIDER_REGISTRY_NAME, "walk");
+    public static final String FANCY_CHICKEN_AMBIENT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.FANCY_CHICKEN_REGISTRY_NAME, "ambient");
+    public static final String FANCY_CHICKEN_HURT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.FANCY_CHICKEN_REGISTRY_NAME, "hurt");
+    public static final String FANCY_CHICKEN_DEATH_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.FANCY_CHICKEN_REGISTRY_NAME, "death");
+    public static final String FANCY_CHICKEN_FLEE_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.FANCY_CHICKEN_REGISTRY_NAME, "flee");
     public static SoundEvent BONE_SPIDER_AMBIENT;
     public static SoundEvent BONE_SPIDER_BONE_STAB;
     public static SoundEvent BONE_SPIDER_DEATH;
     public static SoundEvent BONE_SPIDER_SPIT;
     public static SoundEvent BONE_SPIDER_WALK;
+    public static SoundEvent FANCY_CHICKEN_AMBIENT;
+    public static SoundEvent FANCY_CHICKEN_HURT;
+    public static SoundEvent FANCY_CHICKEN_DEATH;
+    public static SoundEvent FANCY_CHICKEN_FLEE;
 
     public static void init() {
         BONE_SPIDER_AMBIENT = RegisterHelper.registerSoundEvent(BONE_SPIDER_AMBIENT_REGISTRY_NAME);
@@ -23,6 +31,10 @@ public class SoundEventsInit {
         BONE_SPIDER_DEATH = RegisterHelper.registerSoundEvent(BONE_SPIDER_DEATH_REGISTRY_NAME);
         BONE_SPIDER_WALK = RegisterHelper.registerSoundEvent(BONE_SPIDER_WALK_REGISTRY_NAME);
         BONE_SPIDER_SPIT = RegisterHelper.registerSoundEvent(BONE_SPIDER_SPIT_REGISTRY_NAME);
+        FANCY_CHICKEN_AMBIENT = RegisterHelper.registerSoundEvent(FANCY_CHICKEN_AMBIENT_REGISTRY_NAME);
+        FANCY_CHICKEN_HURT = RegisterHelper.registerSoundEvent(FANCY_CHICKEN_HURT_REGISTRY_NAME);
+        FANCY_CHICKEN_DEATH  =RegisterHelper.registerSoundEvent(FANCY_CHICKEN_DEATH_REGISTRY_NAME);
+        FANCY_CHICKEN_FLEE  =RegisterHelper.registerSoundEvent(FANCY_CHICKEN_FLEE_REGISTRY_NAME);
     }
 
     private static String getEntitySoundEventName(String registryName, String type) {
