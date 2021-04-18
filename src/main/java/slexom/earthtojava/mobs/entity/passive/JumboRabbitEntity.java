6a3b320338bafdcc8bbe35ccd09363af8a286599
Carmelo Buscemi <b.carmelo.92@gmail.com>
2021-04-18 14:53:07 +0200

    feat: jumbo rabbbit sounds

--- src/main/java/slexom/earthtojava/mobs/entity/passive/JumboRabbitEntity.java
+++ src/main/java/slexom/earthtojava/mobs/entity/passive/JumboRabbitEntity.java
@@ -1,13 +1,32 @@
 package slexom.earthtojava.mobs.entity.passive;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.World;
 import slexom.earthtojava.mobs.entity.base.E2JBaseRabbitEntity;
+import slexom.earthtojava.mobs.init.SoundEventsInit;
 
 public class JumboRabbitEntity extends E2JBaseRabbitEntity<JumboRabbitEntity> {
 
     public JumboRabbitEntity(EntityType<JumboRabbitEntity> type, World world) {
         super(type, world);
+    }
+
+    @Override
+    protected SoundEvent getJumpSound() {
+        return SoundEventsInit.JUMBO_RABBIT_JUMP;
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return  SoundEventsInit.JUMBO_RABBIT_AMBIENT;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return  SoundEventsInit.JUMBO_RABBIT_HURT;
     }
 
 }
--- src/main/java/slexom/earthtojava/mobs/init/SoundEventsInit.java
+++ src/main/java/slexom/earthtojava/mobs/init/SoundEventsInit.java
@@ -17,6 +17,11 @@ public class SoundEventsInit {
     public static final String FANCY_CHICKEN_FLEE_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.FANCY_CHICKEN_REGISTRY_NAME, "flee");
     public static final String RAINBOW_SHEEP_AMBIENT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.RAINBOW_SHEEP_REGISTRY_NAME, "ambient");
     public static final String RAINBOW_SHEEP_DEATH_REGISTRY_NAME = getEntitySoundEventName( EntityTypesInit.RAINBOW_SHEEP_REGISTRY_NAME, "death");
+
+    public static final String JUMBO_RABBIT_AMBIENT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.JUMBO_RABBIT_REGISTRY_NAME, "ambient");
+    public static final String JUMBO_RABBIT_JUMP_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.JUMBO_RABBIT_REGISTRY_NAME, "jump");
+    public static final String JUMBO_RABBIT_HURT_REGISTRY_NAME = getEntitySoundEventName(EntityTypesInit.JUMBO_RABBIT_REGISTRY_NAME, "hurt");
+
     public static SoundEvent BONE_SPIDER_AMBIENT;
     public static SoundEvent BONE_SPIDER_BONE_STAB;
     public static SoundEvent BONE_SPIDER_DEATH;
@@ -26,6 +31,9 @@ public class SoundEventsInit {
     public static SoundEvent FANCY_CHICKEN_DEATH;
     public static SoundEvent FANCY_CHICKEN_HURT;
     public static SoundEvent FANCY_CHICKEN_FLEE;
+    public static SoundEvent JUMBO_RABBIT_AMBIENT;
+    public static SoundEvent JUMBO_RABBIT_HURT;
+    public static SoundEvent JUMBO_RABBIT_JUMP;
     public static SoundEvent RAINBOW_SHEEP_AMBIENT;
     public static SoundEvent RAINBOW_SHEEP_DEATH;
 
@@ -40,7 +48,10 @@ public class SoundEventsInit {
         FANCY_CHICKEN_HURT = RegisterHelper.registerSoundEvent(FANCY_CHICKEN_HURT_REGISTRY_NAME);
         FANCY_CHICKEN_FLEE  =RegisterHelper.registerSoundEvent(FANCY_CHICKEN_FLEE_REGISTRY_NAME);;
         RAINBOW_SHEEP_AMBIENT = RegisterHelper.registerSoundEvent(RAINBOW_SHEEP_AMBIENT_REGISTRY_NAME);
-        RAINBOW_SHEEP_DEATH  =RegisterHelper.registerSoundEvent( RAINBOW_SHEEP_DEATH_REGISTRY_NAME);
+        RAINBOW_SHEEP_DEATH  = RegisterHelper.registerSoundEvent(RAINBOW_SHEEP_DEATH_REGISTRY_NAME);
+        JUMBO_RABBIT_AMBIENT = RegisterHelper.registerSoundEvent(JUMBO_RABBIT_AMBIENT_REGISTRY_NAME);
+        JUMBO_RABBIT_HURT = RegisterHelper.registerSoundEvent(JUMBO_RABBIT_HURT_REGISTRY_NAME);
+        JUMBO_RABBIT_JUMP = RegisterHelper.registerSoundEvent(JUMBO_RABBIT_JUMP_REGISTRY_NAME);
     }
 
     private static String getEntitySoundEventName(String registryName, String type) {
