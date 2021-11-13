eb62a395fc85d720a8d5d0a72632b256d0679e96
Carmelo Buscemi <b.carmelo.92@gmail.com>
2021-11-13 09:56:44 +0100

    fix: shearable cows drop wool when sheared

--- src/main/java/slexom/earthtojava/mobs/entity/base/E2JBaseShearableCowEntity.java
+++ src/main/java/slexom/earthtojava/mobs/entity/base/E2JBaseShearableCowEntity.java
@@ -18,16 +18,16 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 public class E2JBaseShearableCowEntity<T extends E2JBaseCowEntity<T>> extends E2JBaseCowEntity<T> implements Shearable {
 
     private static final TrackedData<Byte> isSheared = DataTracker.registerData(E2JBaseShearableCowEntity.class, TrackedDataHandlerRegistry.BYTE);
-
+    private final ItemStack wool;
     private int eatGrassTimer;
     private EatGrassGoal eatGrassGoal;
-    private final ItemStack wool;
 
     public E2JBaseShearableCowEntity(EntityType<? extends E2JBaseShearableCowEntity> type, World world, ItemStack wool) {
         super(type, world);
@@ -105,7 +105,7 @@ public class E2JBaseShearableCowEntity<T extends E2JBaseCowEntity<T>> extends E2
     }
 
     public void sheared(SoundCategory shearedSoundCategory) {
-        this.world.playSoundFromEntity( null, this, SoundEvents.ENTITY_SHEEP_SHEAR, shearedSoundCategory, 1.0F, 1.0F);
+        this.world.playSoundFromEntity(null, this, SoundEvents.ENTITY_SHEEP_SHEAR, shearedSoundCategory, 1.0F, 1.0F);
         this.setSheared(true);
         int i = 1 + this.random.nextInt(3);
         for (int j = 0; j < i; ++j) {
@@ -137,17 +137,25 @@ public class E2JBaseShearableCowEntity<T extends E2JBaseCowEntity<T>> extends E2
         } else if (this.eatGrassTimer >= 4 && this.eatGrassTimer <= 36) {
             return 1.0F;
         } else {
-            return this.eatGrassTimer < 4 ? ((float)this.eatGrassTimer - delta) / 4.0F : -((float)(this.eatGrassTimer - 40) - delta) / 4.0F;
+            return this.eatGrassTimer < 4 ? ((float) this.eatGrassTimer - delta) / 4.0F : -((float) (this.eatGrassTimer - 40) - delta) / 4.0F;
         }
     }
 
     @Environment(EnvType.CLIENT)
     public float getHeadAngle(float delta) {
         if (this.eatGrassTimer > 4 && this.eatGrassTimer <= 36) {
-            float f = ((float)(this.eatGrassTimer - 4) - delta) / 32.0F;
+            float f = ((float) (this.eatGrassTimer - 4) - delta) / 32.0F;
             return 0.62831855F + 0.21991149F * MathHelper.sin(f * 28.7F);
         } else {
             return this.eatGrassTimer > 0 ? 0.62831855F : this.getPitch() * 0.017453292F;
+        }
+    }
+
+    public Identifier getLootTableId() {
+        if (this.isSheared()) {
+            return new Identifier("minecraft", "entities/cow");
+        } else {
+            return this.getType().getLootTableId();
         }
     }
 }
