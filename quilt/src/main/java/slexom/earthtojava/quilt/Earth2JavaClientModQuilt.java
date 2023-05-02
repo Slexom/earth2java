3ea6ccc06040bf51bb7321afde3d5ab730233a48
Slexom <b.carmelo.92@gmail.com>
2023-05-02 23:36:46 +0200

    chore: quilt adjustments

--- quilt/src/main/java/slexom/earthtojava/quilt/Earth2JavaClientModQuilt.java
+++ quilt/src/main/java/slexom/earthtojava/quilt/Earth2JavaClientModQuilt.java
@@ -2,41 +2,16 @@ package slexom.earthtojava.quilt;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler;
-import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
-import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.TexturedRenderLayers;
-import net.minecraft.client.texture.Sprite;
-import net.minecraft.client.texture.SpriteAtlasTexture;
-import net.minecraft.fluid.Fluid;
-import net.minecraft.fluid.FluidState;
-import net.minecraft.resource.ResourceManager;
-import net.minecraft.resource.ResourceType;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.BlockRenderView;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import slexom.earthtojava.Earth2JavaClientMod;
-import slexom.earthtojava.Earth2JavaMod;
-
-import java.util.function.Function;
 
 public class Earth2JavaClientModQuilt implements ClientModInitializer {
 
     @Override
     @Environment(EnvType.CLIENT)
     public void onInitializeClient(ModContainer mod) {
-        addBedTextureToAtlas();
         Earth2JavaClientMod.initializeClient();
     }
 
-    private void addBedTextureToAtlas() {
-        Identifier sprite = new Identifier("earthtojavamobs:entity/bed/rainbow");
-        ClientSpriteRegistryCallback.event(TexturedRenderLayers.BEDS_ATLAS_TEXTURE).register((atlasTexture, registry) -> registry.register(sprite));
-    }
 }
