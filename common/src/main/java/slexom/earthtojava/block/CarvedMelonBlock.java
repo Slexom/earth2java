fdc1a07ead50892b868dad2361e4bd3333861ac7
Slexom <b.carmelo.92@gmail.com>
2023-05-02 23:36:19 +0200

    chore: updated carved melon

--- common/src/main/java/slexom/earthtojava/block/CarvedMelonBlock.java
+++ common/src/main/java/slexom/earthtojava/block/CarvedMelonBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.pattern.BlockPattern;
 import net.minecraft.block.pattern.BlockPatternBuilder;
 import net.minecraft.block.pattern.CachedBlockPosition;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.predicate.block.BlockStatePredicate;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -26,7 +27,7 @@ import java.util.function.Predicate;
 
 public class CarvedMelonBlock extends HorizontalFacingBlock {
     public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
-    private static final Predicate<BlockState> IS_MELON = (blockState) -> blockState != null && (blockState.getBlock() == BlockInit.CARVED_MELON || blockState.getBlock() == BlockInit.MELON_LANTERN);
+    private static final Predicate<BlockState> IS_MELON = blockState -> blockState != null && (blockState.getBlock() == BlockInit.CARVED_MELON || blockState.getBlock() == BlockInit.MELON_LANTERN);
     @Nullable
     private BlockPattern snowmanBasePattern;
     @Nullable
@@ -37,9 +38,42 @@ public class CarvedMelonBlock extends HorizontalFacingBlock {
         this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
     }
 
-    public void onBlockAdded(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
-        if (oldState.getBlock() != state.getBlock()) {
-            this.trySpawnGolem(worldIn, pos);
+    public static void spawnEntity(World world, BlockPattern.Result patternResult, Entity entity, BlockPos pos) {
+        breakPatternBlocks(world, patternResult);
+        entity.refreshPositionAndAngles(pos.getX() + 0.5, pos.getY() + 0.05, pos.getZ() + 0.5, 0.0F, 0.0F);
+        world.spawnEntity(entity);
+        for (ServerPlayerEntity serverplayerentity : world.getNonSpectatingEntities(ServerPlayerEntity.class, entity.getBoundingBox().expand(5.0D))) {
+            Criteria.SUMMONED_ENTITY.trigger(serverplayerentity, entity);
+        }
+
+        updatePatternBlocks(world, patternResult);
+    }
+
+    public static void breakPatternBlocks(World world, BlockPattern.Result patternResult) {
+        for (int i = 0; i < patternResult.getWidth(); ++i) {
+            for (int j = 0; j < patternResult.getHeight(); ++j) {
+                CachedBlockPosition cachedBlockPosition = patternResult.translate(i, j, 0);
+                world.setBlockState(cachedBlockPosition.getBlockPos(), Blocks.AIR.getDefaultState(), 2);
+                world.syncWorldEvent(2001, cachedBlockPosition.getBlockPos(), Block.getRawIdFromState(cachedBlockPosition.getBlockState()));
+            }
+        }
+
+    }
+
+    public static void updatePatternBlocks(World world, BlockPattern.Result patternResult) {
+        for (int i = 0; i < patternResult.getWidth(); ++i) {
+            for (int j = 0; j < patternResult.getHeight(); ++j) {
+                CachedBlockPosition cachedBlockPosition = patternResult.translate(i, j, 0);
+                world.updateNeighbors(cachedBlockPosition.getBlockPos(), Blocks.AIR);
+            }
+        }
+
+    }
+
+    @Override
+    public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+        if (!oldState.isOf(state.getBlock())) {
+            this.trySpawnGolem(world, pos);
         }
     }
 
@@ -50,24 +84,10 @@ public class CarvedMelonBlock extends HorizontalFacingBlock {
     private void trySpawnGolem(World world, BlockPos pos) {
         BlockPattern.Result result = this.getSnowmanPattern().searchAround(world, pos);
         if (result != null) {
-            for (int i = 0; i < this.getSnowmanPattern().getHeight(); ++i) {
-                CachedBlockPosition cachedBlockPosition = result.translate(0, i, 0);
-                world.setBlockState(cachedBlockPosition.getBlockPos(), Blocks.AIR.getDefaultState(), 2);
-                world.syncWorldEvent(2001, cachedBlockPosition.getBlockPos(), Block.getRawIdFromState(cachedBlockPosition.getBlockState()));
-            }
-
             MelonGolemEntity melonGolemEntity = EntityTypesInit.MELON_GOLEM_REGISTRY_OBJECT.get().create(world);
-            BlockPos blockPos = result.translate(0, 2, 0).getBlockPos();
-            melonGolemEntity.refreshPositionAndAngles((double) blockPos.getX() + 0.5D, (double) blockPos.getY() + 0.05D, (double) blockPos.getZ() + 0.5D, 0.0F, 0.0F);
-            world.spawnEntity(melonGolemEntity);
 
-            for (ServerPlayerEntity serverplayerentity : world.getNonSpectatingEntities(ServerPlayerEntity.class, melonGolemEntity.getBoundingBox().expand(5.0D))) {
-                Criteria.SUMMONED_ENTITY.trigger(serverplayerentity, melonGolemEntity);
-            }
-
-            for (int l = 0; l < this.getSnowmanPattern().getWidth(); ++l) {
-                CachedBlockPosition cachedBlockInfo3 = result.translate(0, l, 0);
-                world.updateNeighbors(cachedBlockInfo3.getBlockPos(), Blocks.AIR);
+            if (melonGolemEntity != null) {
+                spawnEntity(world, result, melonGolemEntity, result.translate(0, 2, 0).getBlockPos());
             }
         }
     }
@@ -77,6 +97,7 @@ public class CarvedMelonBlock extends HorizontalFacingBlock {
         return this.getDefaultState().with(FACING, context.getHorizontalPlayerFacing().getOpposite());
     }
 
+    @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(FACING);
     }
